4a6c77b4f5ca7b28d4b31a277a24f884ca0ff10f
krish-vadsak45 <krishvadsak234@gmail.com>
2026-01-23 02:18:29 +0530

    endpoint added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
+import axios from "axios";
+
+// 2. Configure Global Settings
+Axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
+axios.defaults.withCredentials = true; // Required if you use cookies/sessions
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
